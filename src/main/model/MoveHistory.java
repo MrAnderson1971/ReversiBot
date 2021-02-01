14c59af9c554915ec1c820b141f239171c1524ec
MrAnderson1971 <mrthomasanderson37@gmail.com>
2021-01-31 18:56:50 -0800

    game history added

--- src/main/model/MoveHistory.java
+++ src/main/model/MoveHistory.java
@@ -24,14 +24,46 @@ public class MoveHistory {
     }
 
     /*
+    EFFECTS: takes integer returns uppercase letter corresponding to it.
+     */
+    private char intToChar(int i) {
+        switch (i) {
+            case 0:
+                return 'A';
+            case 1:
+                return 'B';
+            case 2:
+                return 'C';
+            case 3:
+                return 'D';
+            case 4:
+                return 'E';
+            case 5:
+                return 'F';
+            case 6:
+                return 'G';
+            case 7:
+                return 'H';
+            default:
+                return ' ';
+        }
+    }
+
+    public String moveToString(int[] move) {
+        String s = String.valueOf(intToChar(move[0]));
+        return s + Integer.toString(move[1]);
+    }
+
+    /*
     EFFECTS: returns string representation of this.
      */
     @Override
     public String toString() {
         StringBuilder s = new StringBuilder();
         for (int i = 0; i < moves.size(); i++) {
-            s.append(i).append(". ").append(names.get(i));
-            s.append(" made the move ").append(Arrays.toString(moves.get(i)));
+            s.append(i + 1).append(". ").append(names.get(i));
+            s.append(" made the move ").append(moveToString(moves.get(i)));
+            s.append("\n");
         }
         return s.toString();
     }
---
+++ src/test/model/MoveHistoryTest.java
@@ -0,0 +1,32 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MoveHistoryTest {
+
+    MoveHistory defaultHistory;
+    MoveHistory mh2;
+
+    @BeforeEach
+    void runBefore() {
+        defaultHistory = new MoveHistory();
+        mh2 = new MoveHistory();
+
+        mh2.add(new int[] {1, 2}, "Player 1");
+        mh2.add(new int[] {3, 4}, "Player 2");
+        mh2.add(new int[] {7, 2}, "Player 1");
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("", defaultHistory.toString());
+        assertEquals("1. Player 1 made the move B2\n" +
+        "2. Player 2 made the move D4\n" +
+        "3. Player 1 made the move H2\n", mh2.toString());
+    }
+}
