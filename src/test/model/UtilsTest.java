4979d2f6c6436de7fe4c742e13dc789b79570aab
MrAnderson1971 <mrthomasanderson37@gmail.com>
2021-02-07 19:25:30 -0800

    test count objects

--- src/test/model/UtilsTest.java
+++ src/test/model/UtilsTest.java
@@ -31,4 +31,24 @@ public class UtilsTest {
         assertTrue(Utils.listContainsArray(one, new int[]{3, 4}));
         assertFalse(Utils.listContainsArray(one, new int[] {999, 9998}));
     }
+
+    @Test
+    void testCountObjects() {
+        int[][] array = new int[8][8];
+
+        for (int i = 0; i < 8; i++) {
+            array[0][i] = 1;
+        }
+
+        for (int i = 0; i < 8; i++) {
+            array[i][0] = 2;
+        }
+
+        array[5][5] = 999;
+
+        assertEquals(7, Utils.countObjects(array, 1));
+        assertEquals(8, Utils.countObjects(array,2));
+        assertEquals(1, Utils.countObjects(array, 999));
+        assertEquals(48, Utils.countObjects(array, 0));
+    }
 }
