69ba24f7408b8df743ca511bcd998e4e94b640a2
Thomas Anderson <mrthomasanderson37@gmail.com>
2021-03-01 21:29:56 -0800

    docs

--- src/main/persistence/Writeable.java
+++ src/main/persistence/Writeable.java
@@ -2,6 +2,13 @@ package persistence;
 
 import org.json.JSONObject;
 
+/*
+Subclasses based off of:
+https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo.git
+ */
 public interface Writeable {
+    /*
+    EFFECTS: converts Writeable to JSONObject
+     */
     JSONObject toJson();
 }
